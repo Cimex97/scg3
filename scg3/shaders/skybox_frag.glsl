aceed421aa786183b3da6230aa2076cebc89f33e
Sebasrian Meyer <s3bastianmey@gmail.com>
2019-06-03 10:50:40 +0200

    shader: update skybox fragment shader

--- scg3/shaders/skybox_frag.glsl
+++ scg3/shaders/skybox_frag.glsl
@@ -8,18 +8,25 @@
 smooth in vec3 texCoord0;
 
 uniform samplerCube texture0;
+
+uniform samplerCube texture1;
+
+uniform float blendFactor;
+
 uniform mat4 colorMatrix;
 
 out vec4 fragColor;
 
 
 void main() {
-  vec4 color = texture(texture0, texCoord0);
+  vec4 color1 = texture(texture0, texCoord0);
+  vec4 color2 = texture(texture1, texCoord0);
+  vec4 finalColor = mix(color1,color2,blendFactor);
   
   // transform color by color matrix
-  vec4 transformedColor = colorMatrix * vec4(color.rgb, 1.);
+  vec4 transformedColor = colorMatrix * vec4(finalColor.rgb, 1.);
   transformedColor /= transformedColor.a;   // perspective division
     
   // set final fragment color
-  fragColor = clamp(vec4(transformedColor.rgb, color.a), 0., 1.);
+  fragColor = clamp(vec4(transformedColor.rgb, finalColor.a), 0., 1.);
 }
