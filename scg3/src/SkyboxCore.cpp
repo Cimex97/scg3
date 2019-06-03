b18a25448bb09239d92fe5baf16d28a643219385
Sebasrian Meyer <s3bastianmey@gmail.com>
2019-06-03 10:49:58 +0200

    SkyBox: add a new skybox core

---
+++ scg3/src/SkyboxCore.cpp
@@ -0,0 +1,156 @@
+//
+//  SkyboxCore.cpp
+//  uebung4
+//  Erweiterte CubeMapCore Klasse
+//  Skybox benötigt zwei CubeMaps für Tag / Nacht
+//
+//  Created by Sebastian Meyer on 23.05.19.
+//  Copyright © 2019 Sebastian Meyer. All rights reserved.
+//
+
+#include <cassert>
+#include "SkyboxCore.hpp"
+#include "RenderState.h"
+#include "scg_utilities.h"
+#include "ShaderCore.h"
+
+namespace scg {
+    
+    
+    SkyboxCore::SkyboxCore()
+    : TextureCore(), blendFactor(0.f) {
+    }
+    
+    
+    SkyboxCore::~SkyboxCore() {
+        if (isGLContextActive()) {
+            glActiveTexture(GL_TEXTURE0);
+            glDeleteTextures(1, &tex_);
+            glActiveTexture(GL_TEXTURE1);
+            glDeleteTextures(1, &tex2_);
+            glActiveTexture(GL_TEXTURE0);
+        }
+    }
+    
+    
+    SkyboxCoreSP SkyboxCore::create() {
+        return std::make_shared<SkyboxCore>();
+    }
+    
+    
+    void SkyboxCore::setCubeMap(GLsizei width, GLsizei height, const std::vector<unsigned char*>& rgbaData, GLsizei width2, GLsizei height2, const std::vector<unsigned char*>& rgbaData2) {
+        assert(rgbaData.size() == 6);
+        assert(rgbaData2.size() == 6);
+        
+    // Texture1 in openGL laden
+        glActiveTexture(GL_TEXTURE0);
+        glDeleteTextures(1, &tex_);
+        glGenTextures(1, &tex_);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, tex_);
+        assert(glIsTexture(tex_));
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        
+        // use anisotropic filtering
+        GLfloat maxAnisotropy;
+        glGetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &maxAnisotropy);
+        glTexParameterf(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAX_ANISOTROPY_EXT, maxAnisotropy);
+        
+        // transfer textures to GPU memory
+        const GLenum cubeMapTexNames[] = {
+            GL_TEXTURE_CUBE_MAP_POSITIVE_X, GL_TEXTURE_CUBE_MAP_NEGATIVE_X,
+            GL_TEXTURE_CUBE_MAP_POSITIVE_Y, GL_TEXTURE_CUBE_MAP_NEGATIVE_Y,
+            GL_TEXTURE_CUBE_MAP_POSITIVE_Z, GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+        };
+        for (int i = 0; i < 6; ++i) {
+            assert(rgbaData[i]);
+            glTexImage2D(cubeMapTexNames[i], 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgbaData[i]);
+        }
+        glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+        
+        assert(!checkGLError());
+
+    //Texture2 in openGL laden
+        glActiveTexture(GL_TEXTURE1);
+        glDeleteTextures(1, &tex2_);
+        glGenTextures(1, &tex2_);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, tex2_);
+        assert(glIsTexture(tex2_));
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        
+        // transfer textures to GPU memory
+        for (int i = 0; i < 6; ++i) {
+            assert(rgbaData2[i]);
+            glTexImage2D(cubeMapTexNames[i], 0, GL_RGBA, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgbaData2[i]);
+        }
+        glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+        glActiveTexture(GL_TEXTURE0);
+        assert(!checkGLError());
+    }
+    
+    void SkyboxCore::render(RenderState* renderState) {
+        // multiply current texture matrix by local texture matrix
+        TextureCore::render(renderState);
+    //Texture1 save texture binding
+        glActiveTexture(GL_TEXTURE0);
+        glGetIntegerv(GL_TEXTURE_BINDING_CUBE_MAP, &texOld_);
+        // bind texture
+        assert(glIsTexture(tex_));
+        glBindTexture(GL_TEXTURE_CUBE_MAP, tex_);
+        
+    //Texture2 save texture binding
+        glActiveTexture(GL_TEXTURE1);
+        glGetIntegerv(GL_TEXTURE_BINDING_CUBE_MAP, &texOld2_);
+        // bind texture
+        assert(glIsTexture(tex2_));
+        glBindTexture(GL_TEXTURE_CUBE_MAP, tex2_);
+        
+        // pass inverse view matrix and skybox matrix (i.e., model-view-projection matrix
+        // without camera translation) to shader program
+        glm::mat4 viewMatrix = renderState->getViewTransform();
+        renderState->getShader()->setUniformMatrix4fv("invViewMatrix", 1, glm::value_ptr(glm::inverse(viewMatrix)));
+        viewMatrix[3] = glm::vec4(0.f, 0.f, 0.f, 1.f);
+        glm::mat4 skyboxMatrix = renderState->projectionStack.getMatrix() * viewMatrix * renderState->getModelMatrix();
+        renderState->getShader()->setUniformMatrix4fv("skyboxMatrix", 1, glm::value_ptr(skyboxMatrix));
+    
+        //Day Night Time simple
+        if(blendFactor >= 1){
+            day = false;
+        }else if(blendFactor <= 0){
+            day = true;
+        }
+        
+        if(day){
+            blendFactor+=0.001f;
+        }else{
+            blendFactor-=0.001f;
+        }
+        printf("Blendfactor: %f\n",blendFactor);
+        renderState->getShader()->setUniform1f("blendFactor",  glm::float32(blendFactor));
+        assert(!checkGLError());
+        glActiveTexture(GL_TEXTURE0);
+    }
+    
+    
+    void SkyboxCore::renderPost(RenderState* renderState) {
+        // restore texture binding
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texOld_);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, texOld2_);
+        glActiveTexture(GL_TEXTURE0);
+        // restore texture matrix
+        TextureCore::renderPost(renderState);
+        
+        assert(!checkGLError());
+    }
+    
+    
+} /* namespace scg */
---
+++ scg3/src/SkyboxCore.hpp
@@ -0,0 +1,83 @@
+//
+//  SkyboxCore.hpp
+//  uebung4
+//
+//  Created by Sebastian Meyer on 23.05.19.
+//  Copyright © 2019 Sebastian Meyer. All rights reserved.
+//
+
+#ifndef SkyboxCore_hpp
+#define SkyboxCore_hpp
+
+#include <vector>
+#include "scg_internals.h"
+#include "TextureCore.h"
+SCG_DECLARE_CLASS(SkyboxCore);
+
+namespace scg {
+    /**
+     * \brief Advanced Cube maps textues core.
+     *
+     * To be used in conjunction with cube map or skybox shader.
+     */
+    class SkyboxCore: public TextureCore {
+        
+    public:
+        
+        /**
+         * Constructor.
+         */
+        SkyboxCore();
+        
+        /**
+         * Destructor.
+         */
+        virtual ~SkyboxCore();
+        
+        /**
+         * Create shared pointer.
+         */
+        static SkyboxCoreSP create();
+        
+        /**
+         * Create 2 cube map textures from RGBA images with given parameters.
+         *
+         * \param width texture width
+         * \param height texture height
+         * \param rgbaData vector of 6 arrays of RGBA values for textures in directions
+         *     +x, -x, +y, -y, +z, -z
+         * \param width2 texture2 width
+         * \param height2 texture2 height
+         * \param rgbaData2 vector of 6 arrays of RGBA values for textures in directions
+         *     +x, -x, +y, -y, +z, -z
+         */
+        void setCubeMap(GLsizei width, GLsizei height, const std::vector<unsigned char*>& rgbaData, GLsizei width2, GLsizei height2, const std::vector<unsigned char*>& rgbaData2);
+        
+        // Hilfsklasse zum laden der Texturen
+        void loadTexture(GLuint id, GLsizei width, GLsizei height, const std::vector<unsigned char*>& rgbaData);
+        
+        /**
+         * Render core, i.e., bind texture and post-multiply current texture matrix
+         * by local texture matrix.
+         * Pass inverse view matrix and skybox matrix (i.e., model-view-projection matrix
+         * without camera translation) to shader program.
+         */
+        virtual void render(RenderState* renderState);
+        
+        /**
+         * Render core after traversing sub-tree, i.e., restore previous texture matrix
+         * and bind previous texture.
+         */
+        virtual void renderPost(RenderState* renderState);
+        
+        protected:
+        
+        float blendFactor;
+        bool day;
+        
+    };
+    
+    
+} /* namespace scg */
+
+#endif /* SkyboxCore_hpp */
